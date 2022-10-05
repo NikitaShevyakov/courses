9b9f5ba048dfa86d8d6452a06c968a40e7363552
RedCat <developer.redcat@gmail.com>
2022-10-06 00:06:49 +0300

    Js courses (Practice. Part 4)

--- js/js/script.js
+++ js/js/script.js
@@ -2,67 +2,50 @@
 
 // Место для первой задачи
 function firstTask() {
+    // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+    const arr = [3, 5, 8, 16, 20, 23, 50];
+    const result = [];
+
     // Пишем решение вот тут
-    for(let i=5;i <=10; i++){
-        console.log(i);
-    }   
+    for(var i = 0; i < arr.length; i++){
+        result.push(arr[i]);
+    }
+
+    // Не трогаем
+    return result;
 }
 
 // Место для второй задачи
 function secondTask() {
+    // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+    const data = [5, 10, 'Shopping', 20, 'Homework'];
+
     // Пишем решение вот тут
-     for(let i=20; i >=10; i--){
-        if(i === 13) break;       
-        console.log(i);
-    } 
+    for(var i = 0; i < data.length; i++){
+        let value = data[i];
+        let typeValue = typeof value;
+
+        if(typeValue === 'string'){
+            data[i] = value+=" - done";
+        } else if (typeValue === 'number'){
+            data[i] = value*=2;
+        }
+    }
+    
+    // Не трогаем
+    return data;
 }
 
 // Место для третьей задачи
 function thirdTask() {
+    // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+    const data = [5, 10, 'Shopping', 20, 'Homework'];
+    const result = [];
+
     // Пишем решение вот тут
-    for(let i=1;i <=10; i++){
-        if(i%2 === 0){
-            console.log(i);
-        }
-    }  
-}
-
-// Место для четвертой задачи
-
-// Цикл, который нужно переписать:
-
-// for (let i = 2; i <= 16; i++) {
-//     if (i % 2 === 0) {
-//         continue;
-//     } else {
-//         console.log(i);
-//     }
-// }
-
-function fourthTask() {
-    // Пишем решение вот тут
-    
-    let i = 2;
-    while(i <= 16){
-        if (i % 2 === 0) {
-            i++;
-            continue;
-        } else {
-            console.log(i);
-            i++;
-        }
+    for(var i = data.length; i > 0; i--){    
+        result.push(data[i-1]);
     }
-}
-
-// Место для пятой задачи
-
-function fifthTask() {
-    const arrayOfNumbers = [];
-
-    // Пишем решение вот тут
-   for (let i = 5,j=0; i <= 10; i++,j++) {
-        arrayOfNumbers[j] = i;        
-    }  
     // Не трогаем
-    return arrayOfNumbers;
+    return result;
 }
