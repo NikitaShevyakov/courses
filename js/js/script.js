a30ff4cb9e8add8ae2b6491d4746c9fcb69870ef
RedCat <developer.redcat@gmail.com>
2022-11-15 23:44:13 +0300

    Js courses (Practice. Part 10)

--- js/js/script.js
+++ js/js/script.js
@@ -1,33 +1,41 @@
 'use strict';
 
-function fib(val) {
-    if(typeof(val) !== 'number' || !Number.isInteger){
-        return "";
+const personalPlanPeter = {
+    name: "Peter",
+    age: "29",
+    skills: {
+        languages: ['ru', 'eng'],
+        programmingLangs: {
+            js: '20%',
+            php: '10%'
+        },
+        exp: '1 month'
+    },
+    showAgeAndLangs: function(obj) {
+        let {age} = obj;
+        let {languages} = obj.skills;
+        let result = `Мне ${age} и я владею языками:`;
+        for (let lang in languages){
+            result+= " " + languages[lang].toUpperCase();
+        }
+        return result;
     }
-
-    let number1 = 0;
-    let number2 = 1;
-    let result = '';
-
-    for(let i=0; i < val; i++){
-
-        if(i == 0){
-            result = `${number1}`;
-        } else if (i == 1){
-            result += ` ${number2}`;
-        } else {
-            let newVal = number1 + number2;
-            result += ` ${newVal}`;
     
-            number1 = number2;
-            number2 = newVal;
-        }              
-    }
+};
 
-    return result;
+personalPlanPeter.showAgeAndLangs(personalPlanPeter);
+
+function showExperience(plan) {
+    return plan.skills.exp;
 }
-console.log(fib(4));// ''0 1 1 2"
-console.log(fib(7)); // ''0 1 1 2 3 5 8"
-console.log(fib('7')); // ''"
-console.log(fib(1)); // "0"
-console.log(fib(0)); // ''"
+
+function showProgrammingLangs(plan) {
+    let result = "";
+    let languages = plan.skills.programmingLangs;
+
+    for(let lang in languages){
+        result += `Язык ${lang} изучен на ${languages[lang]}\n`;
+        
+    }
+    return  result;
+}
