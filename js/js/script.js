0944b3fe2cee80f14f19b687f2a37fcc73ed96cd
RedCat <developer.redcat@gmail.com>
2022-12-13 23:31:12 +0300

    * Js courses (Exercise 15)

--- js/js/script.js
+++ js/js/script.js
@@ -1,26 +1,53 @@
 'use strict';
 
-const students = ['Peter', 'Andrew', 'Ann', 'Mark', 'Josh', 'Sandra', 'Cris', 'Bernard', 'Takesi', 'Sam'];
-
-function sortStudentsByGroups(arr) {
-    const a =[] , b = [], c = [], rest = [];
-    arr.sort();
-
-    arr.forEach((item, index) => {
-        
-        if(index < 3){
-            a.push(item);
-        } else if (index < 6){
-            b.push(item);
-        } else if (index < 9){
-            c.push(item);
-        } else {
-            rest.push(item);
+const restorantData = {
+    menu: [
+        {
+            name: 'Salad Caesar',
+            price: '14$'
+        },
+        {
+            name: 'Pizza Diavola',
+            price: '9$'
+        },
+        {
+            name: 'Beefsteak',
+            price: '17$'
+        },
+        {
+            name: 'Napoleon',
+            price: '7$'
         }
+    ],
+    waitors: [
+        {name: 'Alice', age: 22}, {name: 'John', age: 24}
+    ],
+    averageLunchPrice: '20$',
+    openNow: true
+};
 
-        return [a,b,c,`Оставшиеся студенты: ${rest.length === 0 ? '-' : rest.join(', ')}`];
-    });
-    
-    console.log(arr);
+function isOpen(prop) {
+    return prop ? 'Открыто' : 'Закрыто';
 }
-sortStudentsByGroups(students);
+
+console.log(isOpen(restorantData.openNow));
+
+function isAverageLunchPriceTrue(fDish, sDish, average) {
+
+    if ((+fDish.price.slice(0, -1) + +sDish.price.slice(0, -1)) < +average.slice(0, -1)) {
+        return 'Цена ниже средней';
+    } else {
+        return 'Цена выше средней';
+    }
+}
+
+console.log(isAverageLunchPriceTrue(restorantData.menu[3], restorantData.menu[1], restorantData.averageLunchPrice));
+
+function transferWaitors(data) {
+    const copy = Object.assign({}, data);
+
+    copy.waitors = [{name: 'Mike', age: 32}];
+    return copy;
+}
+
+transferWaitors(restorantData);
